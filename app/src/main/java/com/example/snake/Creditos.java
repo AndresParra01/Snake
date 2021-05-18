66f5a0f63666404758c68a9681bef90945d54e00
HugoHernandez06 <44584564+HugoHernandez06@users.noreply.github.com>
2021-05-18 00:20:07 -0500

    Acerca de

--- app/src/main/java/com/example/snake/Creditos.java
+++ app/src/main/java/com/example/snake/Creditos.java
@@ -2,7 +2,9 @@ package com.example.snake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Creditos extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Creditos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creditos);
+    }
+    public void onClick(View v) {
+        Intent nav = new Intent(Creditos.this,MainActivity.class);
+        startActivity(nav);
     }
 }
